97e8fbb1439d1b6cdb933ed5ffb791072d3d8d40
Max Horvath <info@maxhorvath.com>
2010-08-10 15:11:57 +0200

    Modified combining script ...

--- Scripts/CombineLibs.sh
+++ Scripts/CombineLibs.sh
@@ -9,17 +9,17 @@ OUTPUT_FILE=${NAME}${FLAVOR}.a
 ZIP_DIR=${BUILD_DIR}/Zip
 
 if [ ! -d ${OUTPUT_DIR} ]; then
-	mkdir ${OUTPUT_DIR}
+	mkdir -f ${OUTPUT_DIR}
 fi
 
 # Combine lib files
 lipo -create "${BUILD_DIR}/${BUILD_STYLE}-iphoneos/${NAME}Device${FLAVOR}.a" "${BUILD_DIR}/${BUILD_STYLE}-iphonesimulator/${NAME}Simulator${FLAVOR}.a" -output ${OUTPUT_DIR}/${OUTPUT_FILE}
 
-# Copy to direcory for zipping 
-mkdir ${ZIP_DIR}
+# Copy to direcory for zipping
+mkdir -f ${ZIP_DIR}
 cp ${OUTPUT_DIR}/${OUTPUT_FILE} ${ZIP_DIR}
 cp ${BUILD_DIR}/${BUILD_STYLE}-iphonesimulator/*.h ${ZIP_DIR}
-cp ${BUILD_DIR}/${BUILD_STYLE}-iphonesimulator/*.m ${ZIP_DIR}
+#cp ${BUILD_DIR}/${BUILD_STYLE}-iphonesimulator/*.m ${ZIP_DIR}
 #cp ${BUILD_DIR}/${BUILD_STYLE}-iphonesimulator/*.sh ${ZIP_DIR}
 cp ${BUILD_DIR}/${BUILD_STYLE}-iphonesimulator/Makefile ${ZIP_DIR}
 
